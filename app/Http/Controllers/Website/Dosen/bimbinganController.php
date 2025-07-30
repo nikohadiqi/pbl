9b1867aab0dd16a133766cbb7807e9a73fb51304
nico <nikohadiqi@gmail.com>
2025-07-30 22:20:07 +0700

    update

---
+++ app/Http/Controllers/Website/Dosen/bimbinganController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Website\Dosen;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class bimbinganController extends Controller
+{
+    //
+}
--- app/Http/Controllers/Website/Mahasiswa/LogbookController.php
+++ app/Http/Controllers/Website/Mahasiswa/LogbookController.php
@@ -14,7 +14,40 @@ use RealRashid\SweetAlert\Facades\Alert;
 
 class LogbookController extends Controller
 {
-    public function index(Request $request)
+    // public function index(Request $request)
+    // {
+    //     $mahasiswa = Auth::guard('mahasiswa')->user();
+    //     $kodeTim = getKodeTimByAuth();
+
+    //     if (!$kodeTim) {
+    //         return redirect()->back()->with('error', 'Data tim periode aktif tidak ditemukan.');
+    //     }
+
+    //     $logbooks = Logbook::where('kode_tim', $kodeTim)->get();
+
+    //     $timPbl = TimPBL::where('kode_tim', $kodeTim)->first();
+
+    //     $tahapans = TahapanPelaksanaanProyek::where('periode_id', $timPbl->periode)
+    //         ->orderBy('id')
+    //         ->take(16)
+    //         ->get();
+
+    //     $scores = [];
+    //     foreach ($tahapans as $index => $tahapan) {
+    //         $scores[$index + 1] = $tahapan->score ?? 100;
+    //     }
+
+    //     $selectedLogbook = null;
+    //     if ($request->has('selectedId')) {
+    //         $selectedLogbook = Logbook::where('kode_tim', $kodeTim)
+    //             ->where('minggu', $request->input('selectedId'))
+    //             ->first();
+    //     }
+
+    //     return view('mahasiswa.logbook.logbook', compact('logbooks', 'selectedLogbook', 'tahapans', 'scores'));
+    // }
+
+        public function index(Request $request)
     {
         $mahasiswa = Auth::guard('mahasiswa')->user();
         $kodeTim = getKodeTimByAuth();
@@ -54,7 +87,7 @@ class LogbookController extends Controller
     $validated = $request->validate([
         'minggu' => 'nullable|integer|min:1|max:16',
         'aktivitas' => 'required|string|max:255',
-        'hasil' => 'nullable|file|mimes:pdf|max:10240',
+        'hasil' => 'nullable|file|mimes:pdf|max:1080',
         'progress' => 'required|integer|between:0,100',
         'foto_kegiatan' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
         'anggota1' => 'nullable|string|max:255',
